03ca2f7211eb9e0ca20fa7a0dcca03d0d339cee6
russellzparadox <amirhosin775@gmail.com>
2024-08-09 01:24:49 +0330

    god im an idiot fixed the col again

--- view/middleware.py
+++ view/middleware.py
@@ -1,13 +1,14 @@
 from django.utils.deprecation import MiddlewareMixin
-from user_agents import parse
+# from user_agents import parse
 
 
 class DeviceDetectionMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        user_agent = request.META.get('HTTP_USER_AGENT', '')
-        user_agent_obj = parse(user_agent)
-
-        # Add a new attribute to the request object
-        request.is_mobile = user_agent_obj.is_mobile
-        request.is_tablet = user_agent_obj.is_tablet
-        request.is_desktop = user_agent_obj.is_pc
+        pass
+        # user_agent = request.META.get('HTTP_USER_AGENT', '')
+        # user_agent_obj = parse(user_agent)
+        #
+        # # Add a new attribute to the request object
+        # request.is_mobile = user_agent_obj.is_mobile
+        # request.is_tablet = user_agent_obj.is_tablet
+        # request.is_desktop = user_agent_obj.is_pc
